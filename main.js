7db4462989d873f1c0a7e4837ea421c0121f9223
omxgolu <108279138+omxgolu@users.noreply.github.com>
2022-11-27 21:23:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  $(".a").click(function(){
+  $(".clip-top-1").css("background","#000");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#000");
+  $(".btn").css("transition","0.6s")
+  $(".png").css("background","#000");
+  $(".png").css("transition","0.6s")
+  $(".clip-bottom").css("background","#000");
+  });
+  
+  $(".b").click(function(){
+  $(".clip-top-1").css("background","#36B37E");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#36B37E");
+  $(".btn").css("transition","0.6s")
+  $(".png").css("background","#36B37E");
+  $(".png").css("transition","0.6s")
+  $(".clip-bottom").css("background","#36B37E");
+  });
+  
+  $(".c").click(function(){
+  $(".clip-top-1").css("background","#FFAB00");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#FFAB00");
+  $(".btn").css("transition","0.6s")
+  $(".png").css("background","#FFAB00");
+  $(".png").css("transition","0.6s")
+  $(".clip-bottom").css("background","#FFAB00");
+  });
+  
+  $(".d").click(function(){
+  $(".clip-top-1").css("background","#0BC5EA");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#0BC5EA");
+  $(".btn").css("transition","0.6s")
+  $(".png").css("background","#0BC5EA");
+  $(".png").css("transition","0.6s")
+  $(".clip-bottom").css("background","#0BC5EA");
+  });
+  
+  $(".e").click(function(){
+  $(".clip-top-1").css("background","#8954f2");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#8954f2");
+  $(".btn").css("transition","0.6s")
+  $(".png").css("background","#8954f2");
+  $(".png").css("transition","0.6s")
+  $(".clip-bottom").css("background","#8954f2");
+  });
+  
+  $(".f").click(function(){
+  $(".clip-top-1").css("background","#ff1744");
+  $(".clip-top-1").css("transition","0.6s")
+  $(".btn").css("background","#ff1744");
+  $(".btn").css("transition","0.6s")
+  $(".png").css("background","#ff1744");
+  $(".png").css("transition","0.6s")
+  $(".clip-bottom").css("background","#ff1744");
+  });
+  
+});
